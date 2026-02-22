306861dba6d9479c073aa39a609a08b7f304dfab
sreenivas <sreenivaschitti@gmail.com>
2026-02-22 07:32:19 +0530

    terraform

--- variables/variables.tf
+++ variables/variables.tf
@@ -1,18 +1,18 @@
 variable "ami_id" {
-
+    type = string
     default = "ami-0220d79f3f480ecf5"
 
 }
 
 
 variable "instance_type" {
-
+    type = string
     default = "t3.micro"
 
 }
 
 variable "ec2_tags" {
-        
+    type = map    
     default = {
 
         Name = "HelloWorld"
@@ -24,23 +24,23 @@ variable "ec2_tags" {
 
 
 variable "sg_name" {
-
+    type = string
     default = "allow all terraform"
 }
 
 variable "sg_description" {
-
+    type = string
     default = "Allow TLS inbound traffic and all outbound traffic"
 }
 
 variable "sg_from_port" {
-
+    type = number
     default = 0
 
 }
 
 variable "sg_to_port" {
-
+    type = number
     default = 0
 
 }
@@ -48,13 +48,13 @@ variable "sg_to_port" {
 
 
 variable "sg_cidr_blocks" {
-
+    type = list
     default = ["0.0.0.0/0"]
 
 }
 
 variable "sg_tags"{
-
+    type = map
    default = {     
         Name = "aallow all terraform"
      }
